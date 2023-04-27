5d39f3bacbf86b6aef28fc0bd66e1b303f7901b8
Anastasia Lianguzova <anastasialianguzova@gmail.com>
2023-04-27 11:35:54 +0300

    added transrate scripts (rnabloom, rnaspades)

---
+++ Scripts/Host's ganglia transcriptome /TransRate/Rna-Bloom/run_rnabloom_transrate.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+### INPUT ###
+
+R1=/home/LVP/assembly/ganglia.merged.R1.fastq # Full path to the decontaminated and merged R1 fastq file
+R2=/home/LVP/assembly/ganglia.merged.R2.fastq # Full path to the decontaminated and merged R2 fastq file
+Contigs=/home/LVP/assembly/CDHIT_cluster_95_percent/ganglia.new_ref_rnabloom.cdhit_c95.fasta # Full path to the fasta file with assembled contigs (length >= 200 bp)
+OUTDIR=/home/LVP/assembly/QC/rnabloom_TransRate_output # Full path to the directory where the quality control results will be stored
+
+### SOFT ##
+
+source activate /home/lianguzova/mambaforge/envs/transrate_env # Full path to the TransRate virtual environment
+
+TRANSRATE=/home/LVP/Soft/transrate-1.0.1/bin/transrate # Full path to TransRate software
+
+### MAIN ###
+
+echo "***** TransRate (v1.0.1) began to work with params: *****"
+echo "R1: $R1"
+echo "R2: $R2"
+echo "Assembly: $Contigs"
+cd $OUTDIR
+nohup ${TRANSRATE} --assembly=$Contigs --left=$R1 --right=$R2 --threads=24 --output=${OUTDIR}
+wait
+echo "##### Job is complete #####"
