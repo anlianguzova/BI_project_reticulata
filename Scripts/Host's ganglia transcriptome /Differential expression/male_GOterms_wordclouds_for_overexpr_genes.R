b90bca1d2ffd590c66b6c689540187b4247333bb
strongylocentrotus <114506766+strongylocentrotus@users.noreply.github.com>
2023-05-20 14:03:31 +0300

    Add files via upload

---
+++ Scripts/Host's ganglia transcriptome /Differential expression/male_GOterms_wordclouds_for_overexpr_genes.R
@@ -0,0 +1,37 @@
+### Libraries ###
+library(wordcloud)
+library(dplyr)
+library(viridis)
+
+### Input files ###
+infected_reduced <- read.csv2("/home/lisa/Downloads/Telegram Desktop/raki_outputs/Pmin_Infected_male_overexpr_genes_reduced_GSEA_results.Dmelanogaster.tsv", 
+                             header = T, sep="\t")
+healthy_reduced <- read.csv2("/home/lisa/Downloads/Telegram Desktop/raki_outputs/Pmin_Healthy_male_overexpr_genes_reduced_GSEA_results.Dmelanogaster.tsv", 
+                              header = T, sep="\t")
+species_tag <- "Pmin"
+set_tag <- "over-expressed_genes"
+
+set.seed(1234)
+
+diff_terms <- setdiff(healthy_reduced$term, infected_reduced$term)
+diff_reduced <- healthy_reduced[healthy_reduced$term %in% diff_terms, ]
+
+unique(diff_reduced$parentTerm)
+### Processing ###
+input_list <- list("infected_male" = infected_reduced,
+                   "healthy_male" = healthy_reduced,
+                   "differences_male" = diff_reduced)
+
+
+for (set in 1:length(input_list)){
+  set_reduced_terms <- count(input_list[[set]], input_list[[set]]["parentTerm"], sort=T)
+  colnames(set_reduced_terms) <- c("word", "freq")
+  viridis_colors <- viridis(n = length(set_reduced_terms$word))
+  # visual #
+  pdf(file=sprintf("/home/lisa/Downloads/Telegram Desktop/raki_outputs/%s_%s_%s.wordcloud.pdf", species_tag, names(input_list[set]), set_tag), width = 9, height = 9)
+  set_reduced_term_cloud <- wordcloud(words=set_reduced_terms$word, freq=set_reduced_terms$freq, min.freq = 1, 
+                                      max.words=length(set_reduced_terms$word), scale = c(4.25, 0.25),
+                                      random.order = F, rot.per = 0, colors = as.character(viridis_colors), ordered.colors = T)
+  dev.off()
+}
+
